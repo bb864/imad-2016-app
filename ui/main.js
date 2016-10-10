5c4338a0ff0b07cfe791dc7e80b5f7872547d250
bb864 <bhaskarbansal96@gmail.com>
2016-10-11 00:46:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ element.innerHTML = "New Value";
 
 var img = document.getElementById('madi');
 img.onclick = function() {
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveLeft,100);
 };
